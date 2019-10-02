ecf9fc272d905b8dfea98a8d88d6e70e9e0c2437
mydearxym <mydearxym@qq.com>
2019-10-02 18:08:54 +0800

    fix: set link style with max-width

--- src/index.js
+++ src/index.js
@@ -248,7 +248,7 @@ export default class Embed {
       content,
       theme: 'light',
       delay: 200,
-      // trigger: "hover",
+      // trigger: "click",
       placement: 'bottom',
       // allowing you to hover over and click inside them.
       interactive: true,
